68be2a0ca26a3b2aa4b6da3b7a426af81c64d03f
lakshay-0423 <blakshay162@gmail.com>
2026-02-02 20:25:53 +0530

    Week 1 Day 1 - Swap Two Variables.js

---
+++ week 1/day 1/SwapTwoVariables.js
@@ -0,0 +1,8 @@
+let num1=Number(prompt("enter a number"));
+let num2=Number(prompt("enter 2nd number"));
+
+let temp=num1;
+num1=num2;
+num2= temp;
+console.log("num1 = ",num1);
+console.log("num2 = ",num2);
