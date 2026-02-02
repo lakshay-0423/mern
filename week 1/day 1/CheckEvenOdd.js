7947ec5f0521d665a549221519999de84132a2fd
lakshay-0423 <blakshay162@gmail.com>
2026-02-02 20:32:06 +0530

    Week 1 Day 1 - Check even odd using arrow function

---
+++ week 1/day 1/CheckEvenOdd.js
@@ -0,0 +1,11 @@
+let num1=Number(prompt("enter a number"));
+
+const evenodd=(num)=>{
+    if(num%2==0){
+        console.log("even");
+        return;
+    }
+    console.log("odd");
+}
+
+evenodd(num1);
