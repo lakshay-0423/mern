d42e0e82aa860e194f45ca3491981746eb08a2c6
lakshay-0423 <blakshay162@gmail.com>
2026-02-07 20:41:44 +0530

    implement add student command

---
+++ week 1/day 6/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'students.json');
+
+function readStudents() {
+    const data = fs.readFileSync(filePath, 'utf8');
+    return JSON.parse(data);
+}
+
+function writeStudents(data) {
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+}
+
+const action = process.argv[2];
+const name = process.argv[3];
+const age = process.argv[4];
+
+if (action === 'add') {
+    const students = readStudents();
+
+    const newStudent = {
+        id : Date.now(),
+        name : name,
+        age : age
+    }
+
+    students.push(newStudent);
+    writeStudents(students);
+    console.log("student added");
+}
