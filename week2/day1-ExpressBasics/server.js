34cd8b07f1e64bdce1f0251b50adf4f073368812
lakshay-0423 <blakshay162@gmail.com>
2026-02-10 21:30:18 +0530

    add root route returning API running

--- week2/day1-ExpressBasics/server.js
+++ week2/day1-ExpressBasics/server.js
@@ -3,6 +3,10 @@ const express= require('express');
 const app =express();
 const PORT = 5000;
 
+app.get('/',(req,res)=>{
+    res.send("api running");
+})
+
 app.listen(5000,()=>{
     console.log(`Server running on port ${PORT}`);
 })
