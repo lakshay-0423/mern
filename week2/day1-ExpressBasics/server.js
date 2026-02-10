5439866df88629e3b54334c564213b8cfde935b6
lakshay-0423 <blakshay162@gmail.com>
2026-02-10 21:32:38 +0530

    add users API with dummy JSON

--- week2/day1-ExpressBasics/server.js
+++ week2/day1-ExpressBasics/server.js
@@ -1,12 +1,20 @@
-const express= require('express');
+const express = require('express');
 
-const app =express();
+const app = express();
 const PORT = 5000;
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("api running");
 })
 
-app.listen(5000,()=>{
+app.get('/api/users', (req, res) => {
+    res.json([
+        { id: 1, name: "Lakshay" },
+        { id: 2, name: "Aman" },
+        { id: 3, name: "Riya" }
+    ])
+})
+
+app.listen(5000, () => {
     console.log(`Server running on port ${PORT}`);
 })
