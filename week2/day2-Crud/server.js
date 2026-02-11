e9b912b3a5568aa9a94e46374bbc08ad916da4d6
lakshay-0423 <blakshay162@gmail.com>
2026-02-11 21:03:40 +0530

    add express json middleware and students array

--- week2/day2-Crud/server.js
+++ week2/day2-Crud/server.js
@@ -3,6 +3,22 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+app.use(express.json());
+
+let students = [
+    {
+        id: 1,
+        name: "Lakshay",
+        age: 21
+    },
+    {
+        id: 2,
+        name: "Aman",
+        age: 22
+    }
+];
+
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
